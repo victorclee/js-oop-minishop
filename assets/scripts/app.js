13030b44485e1524c63dd899fc4c4643441086f6
victorclee <victorchristopherlee@gmail.com>
2020-03-03 21:35:38 -0600

    getter and setter

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,9 +15,23 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
   addProduct(product) {
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
@@ -87,7 +101,6 @@ class ProductList {
 }
 
 class Shop {
-
   render() {
     const renderHook = document.getElementById('app');
 
@@ -98,7 +111,6 @@ class Shop {
 
     renderHook.append(cartEl);
     renderHook.append(prodListEl);
-
   }
 }
 
