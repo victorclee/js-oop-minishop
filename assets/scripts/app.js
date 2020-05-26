4a2b5ad1c86c82fcf03cdcbf5285f2b3d353e3d8
victorclee <victorchristopherlee@gmail.com>
2020-05-26 14:26:24 -0500

    Private methods.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -120,15 +120,16 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-  products = [];
+  #products = [];
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.render();
     this.fetchProducts();
   }
 
   fetchProducts() {
-    this.products = [
+    this.#products = [
       new Product(
         'A Pillow',
         'assets/images/pillow.jpeg',
@@ -146,7 +147,7 @@ class ProductList extends Component {
   }
 
   renderProducts() {
-    for (const prod of this.products) {
+    for (const prod of this.#products) {
       new ProductItem(prod, 'prod-list');
     }
   }
@@ -155,7 +156,7 @@ class ProductList extends Component {
     this.createRootElement('ul', 'product-list', [
       new elementAttribute('id', 'prod-list')
     ]);
-    if (this.products && this.products.length > 0) {
+    if (this.#products && this.#products.length > 0) {
       this.renderProducts();
     }
   }
